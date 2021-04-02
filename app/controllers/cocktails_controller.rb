f575dc2c3c001ad1f33f9819f1de92745d4add36
Juan Rodriguez <jrcam95@gmail.com>
2021-04-02 17:13:35 -0400

    routed localhost:3000 to cocktails index

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,35 @@
+class CocktailsController < ApplicationController
+    before_action :set_cocktail, only: [:show]
+    def index
+        @cocktails = Cocktail.all
+        @cocktail = Cocktail.new
+    end
+    
+    def show
+    end
+    
+    def new
+        @cocktail = Cocktail.new
+    end
+    
+    def create
+        @cocktail = Cocktail.new(cocktail_params)
+    
+        if @cocktail.save
+          redirect_to @cocktail
+        else
+          render :index
+        end
+    end
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_cocktail
+      @cocktail = Cocktail.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def cocktail_params
+      params.require(:cocktail).permit(:name)
+    end
+    
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+ingredients["drinks"].each do |drink|
+    drink_name = drink["strIngredient1"]
+    new_ingredient = Ingredient.create(name: drink_name)
+    puts "#{new_ingredient.name} created"
+end
